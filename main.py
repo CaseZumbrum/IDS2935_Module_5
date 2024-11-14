849585c6c02c5991e35c34c0a31efbf280804202
Case Zumbrum <casezumbrum@gmail.com>
2024-11-14 11:41:01 -0500

    add pygame support for llm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import speech_recognition as sr
-
+import json
 from transformers import pipeline
 import pygame
 
@@ -7,7 +7,7 @@ import pygame
 
 #pipe = pipeline("text-generation", model="HuggingFaceTB/SmolLM2-135M-Instruct", device="cuda")
 #pipe = pipeline("text-generation", model="HuggingFaceTB/SmolLM2-360M-Instruct", device="cuda")
-pipe = pipeline("text-generation", model="HuggingFaceTB/SmolLM2-1.7B-Instruct", device="cuda")
+pipe = pipeline("text-generation", model="HuggingFaceTB/SmolLM2-1.7B-Instruct")
 
 # Initialize the recognizer 
 r = sr.Recognizer() 
@@ -19,11 +19,20 @@ messages = [
 # Loop infinitely for user to
 # speak
 print("ready")
-pygame.init()
 
-screen = pygame.display.set_mode((1920, 1200))
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((1000, 700))
 pygame.display.set_caption("Design Challenge")
 screen.fill((255,255,255))
+img = pygame.image.load("glasses.jpg")
+img = pygame.transform.scale(img, (1000,700))
+screen.blit(img, (0,0))
+
+pygame.font.init() 
+my_font = pygame.font.SysFont('Times New Roman MS', 20)
+
+
 pygame.display.flip()
 
 while(1):    
@@ -49,8 +58,20 @@ while(1):
             
             messages.append({"role": "user", "content": MyText})
             print("Did you say ",MyText)
-            print(pipe(messages))
-            
+            o = pipe(messages)
+            try:
+                o = json.loads(o)
+                if(type(o) == list and len(o) >= 3):
+                        one_surface = my_font.render('1. ' + o[0], False, (0, 0, 0))
+                        screen.blit(one_surface, (175,250))
+                        one_surface = my_font.render('2. ' + o[1], False, (0, 0, 0))
+                        screen.blit(one_surface, (175,270))
+                        one_surface = my_font.render('3. ' + o[2], False, (0, 0, 0))
+                        screen.blit(one_surface, (175, 290))
+
+                        pygame.display.flip()
+            except json.JSONDecodeError:
+                pass
     except sr.RequestError as e:
         print("Could not request results; {0}".format(e))
         
